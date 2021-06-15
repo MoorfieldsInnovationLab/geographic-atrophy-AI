d85a91fdf4ccc3a2181990cdd9237a0ac55df544
Gongyu Zhang <zgy600@outlook.com>
2021-06-15 19:37:58 +0800

    Upload model source code

--- model.py
+++ model.py
@@ -48,13 +48,13 @@ class AddDoubleConv(nn.Module):
                     nn.BatchNorm2d(out_channels),
                     nn.ReLU(inplace=True)
                 )
-                else:
-                    self.double_conv = nn.Sequential(
-                        nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
-                        nn.ReLU(inplace=True),
-                        nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
-                        nn.ReLU(inplace=True)
-                    )
+            else:
+                self.double_conv = nn.Sequential(
+                    nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+                    nn.ReLU(inplace=True),
+                    nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+                    nn.ReLU(inplace=True)
+                )
 
 
     def forward(self, x):
